72ed8fb5cd5097a0876edfaab351016d655cdec0
AlphaPranav9102 <49816864+AlphaPranav9102@users.noreply.github.com>
2019-05-01 21:31:13 -0400

    Update formjsmessage.js

--- formjsmessage.js
+++ formjsmessage.js
@@ -1,7 +1,7 @@
 submit.onclick = function(){
 	var first_name = document.getElementById("First_Name").value;
 	var last_name = document.getElementById("Last_Name").value;
-  var email_address = document.getElementById("Email_Address").value;
+ 	var email_address = document.getElementById("Email_Address").value;
 	var memo = document.getElementById(memo").value;
   
   const fs = require('fs') 
