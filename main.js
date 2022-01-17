d07073f52c14f311b859549860264c19cf1e65bb
AlphaPranav9102 <49816864+AlphaPranav9102@users.noreply.github.com>
2022-01-17 12:43:03 -0500

    redesign
    Todo: Add contact and tech per project. Add cool stuff tbh.

---
+++ main.js
@@ -0,0 +1,3 @@
+var bigInfo = document.getElementById("bigInfo")
+//bigInfo.style.margin = parseInt(window.innerHeight/4).toString() + 'px 0px 0px ' + parseInt(window.innerWidth/4).toString() + 'px'
+console.log(bigInfo.style.margin)
